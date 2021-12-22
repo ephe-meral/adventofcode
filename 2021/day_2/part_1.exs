d76eec8c8222c84019aaef60a8c823259c5fb1bc
amnesia <amnesia@arcadia.speedport.ip>
2021-12-22 10:30:01 +0100

    add day 2

---
+++ 2021/day_2/part_1.exs
@@ -0,0 +1,17 @@
+#! /usr/bin/env elixir
+
+start = {0, 0}
+
+{end_x, end_y} =
+  File.read!("input.txt")
+  |> String.trim()
+  |> String.split("\n")
+  |> Enum.map(fn x -> String.split(x, " ") end)
+  |> Enum.map(fn [a, b] -> [a, String.to_integer(b)] end)
+  |> Enum.map(fn
+      ["forward", dx] -> {dx, 0}
+      ["down", dy]    -> {0, dy}
+      ["up", dy]      -> {0, -dy} end)
+  |> Enum.reduce(start, fn {dx, dy}, {x, y} -> {x+dx, y+dy} end)
+
+IO.inspect(end_x * end_y)
---
+++ 2021/day_2/part_2.exs
@@ -0,0 +1,17 @@
+#! /usr/bin/env elixir
+
+start = {0, 0, 0} # aim (i.e. slope), x, y
+
+{_end_aim, end_x, end_y} =
+  File.read!("input.txt")
+  |> String.trim()
+  |> String.split("\n")
+  |> Enum.map(fn x -> String.split(x, " ") end)
+  |> Enum.map(fn [a, b] -> [a, String.to_integer(b)] end)
+  |> Enum.map(fn
+      ["forward", dx] -> {0, dx}
+      ["down", da]    -> {da, 0}
+      ["up", da]      -> {-da, 0} end)
+  |> Enum.reduce(start, fn {da, dx}, {a, x, y} -> {a+da, x+dx, y+(dx*a)} end)
+
+IO.inspect(end_x * end_y)

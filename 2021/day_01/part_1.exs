5ce473f4eae13e3c8ac60feb9c124959bbcdc315
ephe-meral <j.appel.online@gmail.com>
2021-12-31 13:15:56 +0100

    refactor day 1

--- 2021/day_01/part_1.exs
+++ 2021/day_01/part_1.exs
@@ -1,15 +1,9 @@
 #! /usr/bin/env elixir
 
-depths =
+IO.inspect(
   File.read!("input.txt")
-  |> String.trim()
-  |> String.split("\n")
+  |> String.split("\n", trim: true)
   |> Enum.map(&String.to_integer/1)
-
-result =
-  Enum.zip(depths, Enum.drop([nil | depths], -1))
-  |> Enum.drop(1)
-  |> Enum.map(fn {a, b} -> ((b-a) < 0) && 1 || 0 end)
-  |> Enum.sum()
-
-IO.inspect(result)
+  |> Enum.chunk_every(2, 1, :discard)
+  |> Enum.filter(fn [x, y] -> x < y end)
+  |> Enum.count())
--- 2021/day_01/part_2.exs
+++ 2021/day_01/part_2.exs
@@ -1,25 +1,11 @@
 #! /usr/bin/env elixir
 
-depths =
+IO.inspect(
   File.read!("input.txt")
-  |> String.trim()
-  |> String.split("\n")
+  |> String.split("\n", trim: true)
   |> Enum.map(&String.to_integer/1)
-
-depths_triplet =
-  Enum.zip([
-    depths,
-    Enum.drop([nil | depths], -1),
-    Enum.drop([nil, nil | depths], -2)])
-  |> Enum.drop(2)
-  |> Enum.map(fn {a, b, c} -> (a+b+c) end)
-
-result =
-  Enum.zip(
-    depths_triplet,
-    Enum.drop([nil | depths_triplet], -1))
-  |> Enum.drop(1)
-  |> Enum.map(fn {a, b} -> ((b-a) < 0) && 1 || 0 end)
-  |> Enum.sum()
-
-IO.inspect(result)
+  |> Enum.chunk_every(3, 1, :discard)
+  |> Enum.map(&Enum.sum/1)
+  |> Enum.chunk_every(2, 1, :discard)
+  |> Enum.filter(fn [x, y] -> x < y end)
+  |> Enum.count())
